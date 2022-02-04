f549c99dad7a7da5118b512607c0506a843e4e41
Neal_Nicdao <chrisnicdao0@gmail.com>
2022-02-04 02:50:50 -0800

    Separate mesh deform code

---
+++ src/mesh_deform.cpp
@@ -0,0 +1,99 @@
+/**
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2022 Neal Nicdao <chrisnicdao0@gmail.com>
+ */
+#include "mesh_deform.hpp"
+
+using namespace meshdeform;
+
+void meshdeform::calculate_joint_transforms(
+        glm::mat4x4 const   &invWorldTf,
+        glm::mat4x4 const   *pInverseBind,
+        glm::mat4x4 const   *pNodeTf,
+        std::size_t const   first,
+        std::size_t const   last,
+        glm::mat4x4         *pJointTfOut) noexcept
+{
+    glm::mat4x4 const *pInverseBindCurr = &pInverseBind[first];
+    glm::mat4x4 const *pNodeTfCurr      = &pNodeTf[first];
+
+    glm::mat4x4 *pJointTfOutCurr = &pJointTfOut[first];
+
+    for (std::size_t i = first; i < last; i ++)
+    {
+        *pJointTfOutCurr = invWorldTf * (*pNodeTfCurr) * (*pInverseBindCurr);
+
+        pInverseBindCurr ++;
+        pNodeTfCurr ++;
+        pJointTfOutCurr ++;
+    }
+}
+
+void meshdeform::apply_vertex_transform(
+        Joints const        &joints,
+        Targets const&      tgt,
+        glm::vec3 const*    pPosIn,
+        glm::vec3 const*    pNrmIn,
+        std::size_t const   first,
+        std::size_t const   last,
+        glm::vec3           *pPosOut,
+        glm::vec3           *pNrmOut) noexcept
+{
+    glm::vec3 *pPosOutCurr = &pPosOut[first];
+    glm::vec3 *pNrmOutCurr = &pNrmOut[first];
+
+    glm::vec3 const *pPosInCurr = &pPosIn[first];
+    glm::vec3 const *pNrmInCurr = &pNrmIn[first];
+
+    float const *pWeightCurr = &joints.m_pWeightsIn[first * 4];
+    unsigned char const *pJointCurr = &joints.m_pJointsIn[first * 4];
+
+    for (std::size_t i = first; i < last; i ++)
+    {
+        glm::vec3 pos(*pPosInCurr);
+        glm::vec3 nrm(*pNrmOutCurr);
+
+        pPosInCurr ++;
+        pNrmInCurr ++;
+
+        // Do targets
+        for (Targets::Target const &tgt : tgt.m_targets)
+        {
+            if (tgt.m_weight == 0)
+            {
+                continue;
+            }
+            pos += tgt.m_pPosIn[i] * tgt.m_weight;
+            nrm += tgt.m_pNrmIn[i] * tgt.m_weight;
+        }
+
+        glm::mat4x4 jointMatrix(0.0f);
+
+        // Do joints
+        for (int j = 0; j < 4; j ++)
+        {
+            float const weight = pWeightCurr[j];
+
+            if (weight == 0.0f)
+            {
+                break;
+            }
+
+            unsigned char const joint = pJointCurr[j];
+
+            jointMatrix += weight * joints.m_jointTf[joint];
+        }
+
+        pos = glm::vec3(jointMatrix * glm::vec4(pos, 1.0f));
+        nrm = glm::vec3(jointMatrix * glm::vec4(nrm, 0.0f));
+
+        pWeightCurr += 4;
+        pJointCurr += 4;
+
+        *pPosOutCurr = pos;
+        *pNrmOutCurr = glm::normalize(nrm);
+
+        pPosOutCurr ++;
+        pNrmOutCurr ++;
+    }
+}
---
+++ src/mesh_deform.hpp
@@ -0,0 +1,58 @@
+/**
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2022 Neal Nicdao <chrisnicdao0@gmail.com>
+ */
+#pragma once
+
+#include <glm/mat4x4.hpp>
+
+#include <vector>
+
+namespace meshdeform
+{
+
+struct Joints
+{
+    glm::mat4x4 const   *m_pInverseBindIn;
+    float const         *m_pWeightsIn;
+    unsigned char const *m_pJointsIn;
+
+    std::vector<glm::mat4x4>    m_nodeTf;
+    std::vector<glm::mat4x4>    m_jointTf;
+};
+
+struct Targets
+{
+    struct Target
+    {
+        glm::vec3 const *m_pPosIn;
+        glm::vec3 const *m_pNrmIn;
+        float m_weight;
+    };
+
+    std::vector<Target> m_targets;
+};
+
+void calculate_joint_transforms(
+        glm::mat4x4 const   &invWorldTf,
+        glm::mat4x4 const   *pInverseBind,
+        glm::mat4x4 const   *pNodeTf,
+        std::size_t const   first,
+        std::size_t const   last,
+        glm::mat4x4         *pJointTfOut) noexcept;
+
+
+
+void apply_vertex_transform(
+        Joints const        &joints,
+        Targets const&      tgt,
+        glm::vec3 const*    pPosIn,
+        glm::vec3 const*    pNrmIn,
+        std::size_t const   first,
+        std::size_t const   last,
+        glm::vec3           *pPosOut,
+        glm::vec3           *pNrmOut) noexcept;
+
+
+
+}
